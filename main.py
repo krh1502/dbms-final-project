3694bfd9221cc6557dea2711726a3ca5f67b43e8
Kate Halushka <katehalushka@gmail.com>
2024-04-13 15:03:56 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Database Mgt Sys Final Project
+# Kate Halushka and Josie Libbon
+# Spring 2024
+
+# Dataset: https://think.cs.vt.edu/corgis/datasets/json/airlines/airlines.json
+
+# TODO: acquire data from internet
+
+# TODO: connect to DB
+
+# TODO: structure data and input into DB
+
+# TODO: perform analysis on data using DB
