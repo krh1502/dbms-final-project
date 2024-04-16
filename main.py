186bbe8e5098128999ad389264ad5e2ca29a6d4d
Kate Halushka <katehalushka@gmail.com>
2024-04-16 13:03:29 -0400

    setup db connection

--- main.py
+++ main.py
@@ -6,6 +6,9 @@
 
 # TODO: acquire data from internet
 import requests, json
+import psycopg2
+import psycopg2.extras
+import pandas as pd
 
 data_endpoint = 'https://think.cs.vt.edu/corgis/datasets/json/airlines/airlines.json'
 
@@ -13,6 +16,16 @@ unstructured_data = requests.get(data_endpoint)
 json_data = unstructured_data.json()
 
 # TODO: connect to DB
+
+# Connect to the database
+connection = psycopg2.connect(host='dbms-final-project.cb6yyy428jlt.us-east-1.rds.amazonaws.com',
+                             user='postgres',
+                             port=5432,
+                             database='airportdata',
+                             password='dbstudent')
+connection.set_session(readonly=True, autocommit=True)
+
+cursor = connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
 
 # TODO: structure data and input into DB
 
